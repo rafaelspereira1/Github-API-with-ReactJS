11b8894d8afe3976d88ae268f0249951a30f0a96
Rafael Pereira <rafaelspereira1@hotmail.com>
2021-10-20 23:29:34 -0300

    Profile to component

--- github-api-with_reactjs/src/App.js
+++ github-api-with_reactjs/src/App.js
@@ -1,29 +1,12 @@
 import React from "react";
-import Layout from "./layout";
-function App() {
+import Layout from "./components/layout";
+import Profile from "./components/profile";
+
+const App = () => { 
   return (
     <main>
     <Layout>
-      <div>
-        <img source="https://avatars.githubusercontent.com/u/90010200?v=4" alt="Avatar of User"></img>
-        <h1>Rafael Pereira</h1>
-        <h3>Username:</h3>
-        <span>rafaelspereira1</span>
-        <div>
-          <div>
-          <h4>Followers</h4>
-          <span>5</span>
-          </div>
-          <div>
-          <h4>Starred</h4>
-          <span>5</span>
-          </div>
-          <div>
-          <h4>Following</h4>
-          <span>5</span>
-          </div>
-        </div>
-      </div>
+    <Profile />
       <div>Repositories</div>
       <div>Starred</div>
     </Layout>
