282a0a20aa0234a5aad95ad3ff6bff2a18ead167
Rafael Pereira <rafaelspereira1@hotmail.com>
2021-10-21 21:19:50 -0300

    Update styled.js

--- github-api-with_reactjs/src/components/profile/styled.js
+++ github-api-with_reactjs/src/components/profile/styled.js
@@ -8,14 +8,33 @@ export const Wrapper = styled.div`
 export const WrapperStatusCount = styled.div`
   display: flex;
   align-items: flex-start;
+  div {
+    margin: 8px;
+    text-align: center;
+  }
 `;
 
 export const WrapperStatusUsername = styled.div`
   display: flex;
   align-items: flex-start;
+  align-items: center
 `;
 
 export const WrapperInfoUser = styled.div`
   display: flex;
   align-items: flex-start;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  margin-left: 8px;
+
+  h3 {
+    margin: 8px;
+  }
+`;
+
+export const WrapperImage = styled.img`
+border-radius: 50%;
+width: 180px;
+margin: 25px;
 `;
