b27eb691e72f9a52a979b81f4c47cdc4662ff8e9
Rafael Pereira <rafaelspereira1@hotmail.com>
2021-10-25 13:24:21 -0300

    Bug fix

--- github-api-with_reactjs/src/components/repositories/index.js
+++ github-api-with_reactjs/src/components/repositories/index.js
@@ -1,17 +1,30 @@
 import React from 'react'
+import RepositoryItem from '../repository-item/index.js';
 import * as S from './styled.js';
 
 const Repositories = () => {
     return (
         <S.WrapperTabs
-            selectedTTabClassName="is-selected" 
+            selectedTabClassName="is-selected" 
             selectedTabPanelClassName="is-selected">
                 <S.WrapperTabList>
                     <S.WrapperTab>Repositories</S.WrapperTab>
                     <S.WrapperTab>Starred</S.WrapperTab>
                 </S.WrapperTabList>
-                <S.WrapperTabPanel>panel Repositories</S.WrapperTabPanel>
-                <S.WrapperTabPanel>panel Starred</S.WrapperTabPanel>
+                <S.WrapperTabPanel>
+                    <RepositoryItem
+                        name="Repo 1"
+                        linkToRepo="https://github.com/rafaelspereira1"
+                        fullName="teste/teste"
+                    />
+                </S.WrapperTabPanel>
+                <S.WrapperTabPanel>
+                <RepositoryItem
+                        name="Repo 1"
+                        linkToRepo="https://github.com/rafaelspereira1-2"
+                        fullName="teste/teste2"
+                    />
+                </S.WrapperTabPanel>
             
         </S.WrapperTabs>
     )
