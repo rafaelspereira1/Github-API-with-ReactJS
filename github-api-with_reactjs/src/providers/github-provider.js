f2abf00e145cc3423c5b1b086614f02fb1e01387
Rafael Pereira <rafaelspereira1@hotmail.com>
2021-10-25 13:24:54 -0300

    Create github-provider.js

---
+++ github-api-with_reactjs/src/providers/github-provider.js
@@ -0,0 +1,38 @@
+import React, {createContext, useState} from 'react'
+
+export const GithubContext = createContext ({
+    user: {},
+    repositories: [],
+    starred: [],
+})
+
+const GithubProvider = ({children}) => {
+    const [githubState, setGithubState] = useState({
+        user: {
+            id: undefined,
+            avatar: undefined,
+            login: undefined,
+            name: undefined,
+            html_url: undefined,
+            blog: undefined,
+            company: undefined,
+            location: undefined,
+            followers: 0,
+            following: 0,
+            public_gists: 0,
+            public_repos: 0,
+          },
+        repositories: [],
+        starred: [],
+    });
+    
+    const contextValue = {
+        githubState,
+    }
+
+    return (
+        <GithubContext.Provider value={contextValue}>{children}</GithubContext.Provider>
+    )
+}
+
+export default GithubProvider
