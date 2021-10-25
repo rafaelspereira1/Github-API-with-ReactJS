758b522e2eecc1f1361c3e37eba24fdc4336d989
Rafael Pereira <rafaelspereira1@hotmail.com>
2021-10-25 13:24:46 -0300

    Bug fix

--- github-api-with_reactjs/src/components/repository-item/index.js
+++ github-api-with_reactjs/src/components/repository-item/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Repositoryitem = () => {
+const RepositoryItem = ({name, linkToRepo, fullName}) => {
     return (
         <div>
-            
+            <h2>{name}</h2>
+            <h4>full name:</h4>
+            <a href={linkToRepo} target="_blank" rel="noreferrer">{fullName}</a>
         </div>
     )
 }
 
-export default Repository-item;
+export default RepositoryItem;
