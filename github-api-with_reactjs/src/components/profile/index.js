3f1146082eab338de45b175d12928dadb9d509ea
Rafael Pereira <rafaelspereira1@hotmail.com>
2021-10-25 17:15:43 -0300

    Change mockup data to actual data

--- github-api-with_reactjs/src/components/profile/index.js
+++ github-api-with_reactjs/src/components/profile/index.js
@@ -1,30 +1,38 @@
 import React from "react";
+import useGithub from "../../hooks/github-hooks";
 import * as S from "./styled";
 
 const Profile = () => {
+
+   const { githubState} = useGithub();
+
     return (
 <S.Wrapper>
    <S.WrapperImage src="https://avatars.githubusercontent.com/u/90010200?v=4" alt="Avatar of User"></S.WrapperImage>
    <S.WrapperInfoUser>
    <div>
-      <h1>Rafael Pereira</h1>
+      <h1>{githubState.user.name}</h1>
       <S.WrapperStatusUsername>
          <h3>Username:</h3>
-         <a href="" target="_blank" rel="noreferrer">rafaelspereira1</a>
+         <a href={githubState.user.html_url} target="_blank" rel="noreferrer">{githubState.user.login}</a>
       </S.WrapperStatusUsername>
    </div>
    <S.WrapperStatusCount>
       <div>
          <h4>Followers</h4>
-         <span>5</span>
-      </div>
-      <div>
-         <h4>Starred</h4>
-         <span>5</span>
+         <span>{githubState.user.followers}</span>
       </div>
       <div>
          <h4>Following</h4>
-         <span>5</span>
+         <span>{githubState.user.following}</span>
+      </div>
+      <div>
+         <h4>Gists</h4>
+         <span>{githubState.user.public_gists}</span>
+      </div>
+      <div>
+         <h4>Repos</h4>
+         <span>{githubState.user.public_repos}</span>
       </div>
    </S.WrapperStatusCount>
    </S.WrapperInfoUser>
